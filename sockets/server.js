68d20bb55d72c9739b0fd9e294e31d62d3b98fa5
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-09-23 02:09:03 -0500

    First socket connection.

---
+++ sockets/server.js
@@ -0,0 +1,19 @@
+
+// Server with Express
+const express = require('express');
+const app = express();
+
+const http = require('http');
+
+const server = http.createServer(app);
+
+server.listen(8080);
+
+// Listen connections with socketIO:
+const socketIo = require('socketIo');
+const io = socketIo.listen(server);
+
+// Listen the event "connect" and run the function
+io.listen('connect', function(socket){
+    console.log('new connection id:'+ socket.id);
+});
