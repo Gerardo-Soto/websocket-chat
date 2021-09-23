c59e2b3033742416f01a528083d66831b38167ff
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-09-23 02:25:18 -0500

    Initialization (Documentation).

--- sockets/server.js
+++ sockets/server.js
@@ -7,13 +7,15 @@ const http = require('http');
 
 const server = http.createServer(app);
 
-server.listen(8080);
+server.listen(8081);
 
 // Listen connections with socketIO:
-const socketIo = require('socket.io');
-const io = socketIo.listen(server);
+const {socketIo} = require("socket.io");
+const io = new socketIo();
 
 // Listen the event "connect" and run the function
 io.on('connect', function(socket){
     console.log('new connection id:'+ socket.id);
 });
+
+io.listen(8080);
