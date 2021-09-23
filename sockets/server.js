e618231a8090c74ffc08b8fc0ee5dc98ad08b303
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-09-23 02:37:35 -0500

    Show our index.html with express.

--- sockets/server.js
+++ sockets/server.js
@@ -8,6 +8,10 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
+
+// Show our index.html 
+app.use(express.static('public'));
+
 io.on("connection", (socket) => {
     // ...
     console.log('new connection id:'+ socket.id);
