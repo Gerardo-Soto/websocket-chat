89c8e9aa6e7143fd95455f48d494c58081076d49
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-09-23 13:25:36 -0500

    Used emit to send an alert when a new user is conected.

--- sockets/server.js
+++ sockets/server.js
@@ -15,6 +15,21 @@ app.use(express.static('public'));
 io.on("connection", (socket) => {
     // ...
     console.log('new connection id:'+ socket.id);
+
+    //io.on('userdata', function(data){     //test
+    socket.on('userdata', function(data){
+        console.log('email: '+ data.email +' user: '+ data.user);
+    });
+
+
+    // Server response
+    socket.on('userdata', function(data){
+        console.log('email: '+ data.email +' user: '+ data.user);
+        // Send a emit to all users connected:
+        io.emit('[Server] New user connected, hi, I am: '+ data.user, {user: data.user});
+    });
+
+
 });
 
 httpServer.listen(8080);
